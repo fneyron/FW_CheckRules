f778e6cad8c705d296c875c4ba69320918c5b7d2
NEYRON Florent <florent.neyron@gmail.com>
2015-01-20 17:09:13 +0100

    modified:   createRule.sh

--- createRule.sh
+++ createRule.sh
@@ -21,9 +21,19 @@ function chck_service(){
 	fi
 }
 
+
+function default(){
+        echo -e "$0 [-c|-e|-l]"
+        echo -e "\t -c : Create a new group"
+        echo -e "\t -e : Erase a group"
+        echo -e "\t -l : List groups"
+}
+
+if [ "$1" == "-c" ]
+then
 clear
 mysql -u root -D fw_logs -e "select distinct grpName from fwGroup;"
-echo -e "Enter Source (group or any) : \n"
+echo -e "Enter Source (group, ip or any) and finish ".": \n"
 read source
 chck_group $source
 
@@ -47,4 +57,18 @@ if [ "$rule" == "" ]; then rule=$(($(mysql -u root -N -s -D fw_logs -e "select m
 mysql -u root -D fw_logs -e "insert into fwRule values('', '$source', '$destination', '$srvc_grp', '$policy', '$rule');"
 mysql -u root -D fw_logs -e "select * from fwRule order by Rule;"
 	
+elif [ "$1" == "-l" ]
+then
+	mysql -u root -D fw_logs -e "select Rule, Source, Destination, Service, Policy from fwRule order by Rule;"
+	echo "Give a rule number: "
+	read rule
+	echo "Group Source:"
+	mysql -u root -D fw_logs -e "select grpName, ipRange from fwGroup g, fwRule f where g.grpName=f.Source and f.Rule=$rule;" 
+	echo "Group Destination:"
+	mysql -u root -D fw_logs -e "select grpName, ipRange from fwGroup g, fwRule f where g.grpName=f.Destination and f.Rule=$rule;"
+	echo "Service Group:"
+	mysql -u root -D fw_logs -e "select srvcName, protocol, port_b, port_e from fwService s, fwRule f where s.srvcName=f.Service and f.Rule=$rule;"
+else 
+	default
+fi
 
